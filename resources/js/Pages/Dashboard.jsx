67a040065e4a227c9d3e5355854e5554a841d24d
fatihaljabar <fatihaljabar@gmail.com>
2023-06-18 11:56:45 +0700

    feat: integration dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,14 +1,15 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import React from 'react';
+import Authenticated from '@/Layouts/Authenticated/Index';
 import { Head } from '@inertiajs/react';
 
-export default function Dashboard({ auth }) {
+export default function Dashboard({ props }) {
     return (
-        <AuthenticatedLayout
-            user={auth.user}
+        <Authenticated
+            auth={props.auth}
+            errors={props.errors}
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
         >
             <Head title="Dashboard" />
-
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -16,6 +17,6 @@ export default function Dashboard({ auth }) {
                     </div>
                 </div>
             </div>
-        </AuthenticatedLayout>
+        </Authenticated>
     );
 }
